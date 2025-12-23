bbc8b94ae5eb179752886a3d975ced368701b3b1
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2025-12-22 21:42:46 -0500

    Comment Out notifications button

--- components/DashboardHeader/DashboardHeader.tsx
+++ components/DashboardHeader/DashboardHeader.tsx
@@ -283,13 +283,13 @@ export default function DashboardHeader({ title, actionButton, onSearch, onProje
         )}
         
         {/* Notifications */}
-        <button className={styles.notificationButton} aria-label="Notifications">
+        {/* <button className={styles.notificationButton} aria-label="Notifications">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M13.73 21a2 2 0 0 1-3.46 0" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
           <div className={styles.notificationBadge}>3</div>
-        </button>
+        </button> */}
 
         {/* User Menu */}
         {user && (
